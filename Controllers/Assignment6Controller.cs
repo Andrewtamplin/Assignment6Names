c29d95cd219df8f21887f34dce55a1b0afe23db3
tamplinandrew <tampliaw@mail.uc.edu>
2021-03-29 15:04:22 -0400

    New Name

---
+++ Controllers/Assignment6Controller.cs
@@ -0,0 +1,31 @@
+using Assignment6Names.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Assignment6Names.AddControllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Assignment6Controller : ControllerBase
+    {
+        private ILogger<Assignment6Controller> _logger;
+
+        
+        public Assignment6Controller(ILogger<Assignment6Controller> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public Assignment6 Get() 
+        {
+            return new Assignment6
+            {
+                firstName = "George",
+                lastName = "Washington",
+                major = "Politcs"
+            };
+        }
+
+    }
+}
